70f7c7018b28cf4d17238b3639b2f41249c701a7
EC2 Default User <ec2-user@ip-172-31-41-117.ec2.internal>
2019-11-25 01:15:36 +0000

    added grade configs to repo

--- src/test/java/MAST/HelloControllerTest.java
+++ src/test/java/MAST/HelloControllerTest.java
@@ -1,24 +1,29 @@
-// @RunWith(SpringJUnit4ClassRunner.class)
-// @ContextConfiguration(classes = { HelloLambdaApplication.class })
-// @WebAppConfiguration
-// public class HelloControllerTest {
-//     private MockLambdaContext lambdaContext;
-//     private SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
-    
-//     @Autowired 
-//     private ObjectMapper mapper;
- 
-//     public HelloControllerTest() { 
-//       lambdaContext = new MockLambdaContext(); 
-//       this.handler = StreamLambdaHandler.handler; 
-//     }
+package MAST;
+import org.junit.runner.Runwith;
+import com.fasterxml.jackson.core.JsonParseException;
 
-//     @Test public void testGreetingApi() throws JsonParseException, JsonMappingException, IOException {
-//       AwsProxyRequest request = new AwsProxyRequestBuilder("/greeting", "GET").queryString("name", "John").build(); 
-//       AwsProxyResponse response = handler.proxy(request, lambdaContext);
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { HelloLambdaApplication.class })
+@WebAppConfiguration
+public class HelloControllerTest {
+    private MockLambdaContext lambdaContext;
+    private SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+    
+    @Autowired 
+    private ObjectMapper mapper;
+ 
+    public HelloControllerTest() { 
+      lambdaContext = new MockLambdaContext(); 
+      this.handler = StreamLambdaHandler.handler; 
+    }
+
+    @Test public void testGreetingApi() throws JsonParseException, JsonMappingException, IOException {
+      AwsProxyRequest request = new AwsProxyRequestBuilder("/profile", "GET").queryString("name", "John").build(); 
+      AwsProxyResponse response = handler.proxy(request, lambdaContext);
       
-//       assertThat(response.getStatusCode(), equalTo(200)); 
-//       GreetingDto responseBody = mapper.readValue(response.getBody(), GreetingDto.class);
-//       asserThat(responseBody.getMessage(), equalTo("Hello John")); 
-//     }
-// }
+      assertThat(response.getStatusCode(), equalTo(200)); 
+      GreetingDto responseBody = mapper.readValue(response.getBody(), GreetingDto.class);
+      asserThat(responseBody.getMessage(), equalTo("Hello John")); 
+    }
+}
